8d2512e1a32406e8ab21fa0bfa7578d356ed97d7
Роман <romanze1706@gmail.com>
2022-11-15 21:23:34 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int pay = 0;
+        int[] arr = generateRandomArray();
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.println(i + " " + arr[i]);
+            pay += arr[i];
+        }
+
+        System.out.println("Сумма всех выплат за месяц: " + pay);
+    }
+
+    public static int[] generateRandomArray() {
+
+        Random random = new Random();
+        int[] arr = new int[30];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
+
